935c8e4b290010310beac4fbfb45d2cca1bccee9
Peter Klipfel <peter@klipfel.me>
2013-11-04 10:36:23 -0700

    scrolling only on links that make sense to correct position

--- js/extras.js
+++ js/extras.js
@@ -19,13 +19,13 @@ $( function() {
     $(this).tab('show')
   })
   $(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
+    $('.scrollTo').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
         if (target.length) {
           $('html,body').animate({
-            scrollTop: target.offset().top
+            scrollTop: target.offset().top-60
           }, 1000);
           return false;
         }

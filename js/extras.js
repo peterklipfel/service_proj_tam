3ed4601d80a6034bce124c7683018cf18b44d3b2
Peter Klipfel <peter@klipfel.me>
2013-11-09 21:20:36 -0700

    content and scrollspy;

--- js/extras.js
+++ js/extras.js
@@ -2,10 +2,32 @@ $( function() {
   $('.navbar-fixed-top').hide()
   var containerHeight = $('.top-frame').height()
   $(document).scroll(function(){
-    if($(document).scrollTop() > containerHeight)
-      $('.navbar-fixed-top').fadeIn(300);
-    else
-      $('.navbar-fixed-top').fadeOut(300)
+    var top = $(document).scrollTop()
+    if(top > containerHeight/1.4){ $('.navbar-fixed-top').fadeIn(300) }
+    else { $('.navbar-fixed-top').fadeOut(300) }
+
+    switch(true){
+      case (top > $('#faq-section').position().top+$('#faq-section').height()/3):
+        $('#nav-signup').siblings().removeClass('active')
+        $('#nav-signup').addClass('active')
+        console.log($('#nav-about').siblings())
+        break;
+      case (top > $('#faq-section').position().top):
+        $('#nav-faq').siblings().removeClass('active')
+        $('#nav-faq').addClass('active')
+        console.log('2')
+        break;
+      case (top > $('#events-section').position().top):
+        $('#nav-events').siblings().removeClass('active')
+        $('#nav-events').addClass('active')
+        console.log('3')
+        break;
+      case (top > $('#about-section').position().top):
+        $('#nav-about').siblings().removeClass('active')
+        $('#nav-about').addClass('active')
+        console.log($('#nav-about').siblings())
+        break;
+    }
   })
   if( ($(document).width() > 767) && parseInt($('#about-text').height()) > 600 ){
     $('.about-image').css('margin-top', 
